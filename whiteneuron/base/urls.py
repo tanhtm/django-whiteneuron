e310879c8683495ae90efc6674f12401ed2858c0
Nguyễn Tú Anh <ntanhtm@gmail.com>
2025-04-24 11:50:41 +0700

    Build Thu Apr 24 11:50:34 +07 2025: static

--- whiteneuron/base/urls.py
+++ whiteneuron/base/urls.py
@@ -9,8 +9,7 @@ from .sites import base_admin_site
 from .views import HomeView
 
 urlpatterns = []
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += [
